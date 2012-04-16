dc88d2a76525839912c232b7bddc58bf8ca8a986
Carlo Tasca <carlo.tasca.mail@gmail.com>
2012-04-16 19:26:35 +0100

    Fixed some styling warnings

--- src/MyClass.php
+++ src/MyClass.php
@@ -1,17 +1,14 @@
 <?php
 class MyClass {
-	
-    public function demo($a) {
+   public function demo($a) {
         if($a == 1) {
             return 1;
         }
         return 0;
-    }
-	
+	}
 	public function testing_that () {
 		return false;
 	}
-	
 	public function testingThis () {
 		return false;
 	}
